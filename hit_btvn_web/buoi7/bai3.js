391a2ae01e9dc89578bb397b2cea54bad8edd104
Hoàng Minh Huệ <92368781+Hue2k9@users.noreply.github.com>
2021-12-07 00:05:19 +0700

    Update bai3.js

--- hit_btvn_web/buoi7/bai3.js
+++ hit_btvn_web/buoi7/bai3.js
@@ -5,8 +5,8 @@ function Post(title, body, author, views, comments, isLive){
     this.comments=comments;
     this.isLive=isLive;
 }
-const show = new Post("ahihi","anh Hoàng đzzz","abc",100,[
+const post = new Post("ahihi","anh Hoàng đzzz","abc",100,[
     {author: 'anh Huân đzzz', body: "lewlew"},
     {author: 'anh Huân đzzz', body: "lewlew"},
   ],true);
-console.log(show);
+console.log(post);
